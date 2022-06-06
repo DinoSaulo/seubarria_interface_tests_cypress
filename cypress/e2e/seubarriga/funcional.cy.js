ea8b8c2ca532c559fea7b028608cafbedf139e14
dinosaulo <sab2@cin.ufpe.br>
2022-06-06 05:13:46 -0300

    minor: starting repo

---
+++ cypress/e2e/seubarriga/funcional.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+import '../../support/commandsContas'
+
+describe('Should test at an interface level', () => {
+
+    let token
+
+    before( () => {
+        
+    })
+
+    beforeEach(() => {
+    })
+
+
+})
